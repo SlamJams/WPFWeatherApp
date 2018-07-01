f83c05a00b2041273b2f4a977bdc921b38636df1
Brian Najera <brian2najera@gmail.com>
2018-07-01 00:33:45 -0500

    Setted up MVVM

---
+++ WPFWeatherApp/Model/AccuWeather.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFWeatherApp.Model
+{
+    public class Metric
+    {
+        public double Value { get; set; }
+        public string Unit { get; set; }
+        public int UnitType { get; set; }
+    }
+
+    public class Imperial
+    {
+        public int Value { get; set; }
+        public string Unit { get; set; }
+        public int UnitType { get; set; }
+    }
+
+    public class Temperature
+    {
+        public Metric Metric { get; set; }
+        public Imperial Imperial { get; set; }
+    }
+
+    public class AccuWeather
+    {
+        public DateTime LocalObservationDateTime { get; set; }
+        public int EpochTime { get; set; }
+        public string WeatherText { get; set; }
+        public int WeatherIcon { get; set; }
+        public bool IsDayTime { get; set; }
+        public Temperature Temperature { get; set; }
+        //public string MobileLink { get; set; }
+        //public string Link { get; set; }
+    }
+}
---
+++ WPFWeatherApp/ViewModel/WeatherAPI.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using WPFWeatherApp.Model;
+using Newtonsoft.Json;
+
+namespace WPFWeatherApp.ViewModel
+{
+    class WeatherAPI
+    {
+        public const string API_KEY = "pIdABsEBSEAllimmMYA4EtdkxJukxDzB";
+        public const string BASE_URL = "http://dataservice.accuweather.com/currentconditions/v1/351197?apikey={0}";
+
+        public async Task<AccuWeather> GetWeatherInformationAsync(string cityName)
+        {
+            AccuWeather result = new AccuWeather();
+
+            string url = String.Format(BASE_URL, API_KEY);
+
+            using (HttpClient client = new HttpClient())
+            {
+                var response = await client.GetAsync(url);
+                string json = await response.Content.ReadAsStringAsync();
+
+                result = JsonConvert.DeserializeObject<AccuWeather>(json);
+            }
+
+            return result;
+        }
+    }
+}

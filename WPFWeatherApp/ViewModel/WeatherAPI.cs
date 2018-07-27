def67a166698f986da77805b89b7ddacff9ed327
Brian Najera <brian2najera@gmail.com>
2018-07-26 19:36:43 -0500

    Fixed Getting cities async

--- WPFWeatherApp/ViewModel/WeatherAPI.cs
+++ WPFWeatherApp/ViewModel/WeatherAPI.cs
@@ -13,6 +13,7 @@ namespace WPFWeatherApp.ViewModel
     {
         public const string API_KEY = "pIdABsEBSEAllimmMYA4EtdkxJukxDzB";
         public const string BASE_URL = "http://dataservice.accuweather.com/currentconditions/v1/351197?apikey={0}";
+        public const string BASE_URL_AUTOCOMPLETE = "http://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey={0}&q={1}";
 
         public async Task<AccuWeather> GetWeatherInformationAsync(string cityName)
         {
@@ -29,6 +30,23 @@ namespace WPFWeatherApp.ViewModel
             }
 
             return result;
+        }
+
+        public static async Task<List<City>> GetAutoCompleteAsync(string query)
+        {
+            List<City> cities = new List<City>();
+
+            string url = String.Format(BASE_URL_AUTOCOMPLETE, API_KEY, query);
+
+            using (HttpClient client = new HttpClient())
+            {
+                var response = await client.GetAsync(url);
+                string json = await response.Content.ReadAsStringAsync();
+
+                cities = JsonConvert.DeserializeObject<List<City>>(json);
+            }
+
+            return cities;
         }
     }
 }
--- WPFWeatherApp/ViewModel/WeatherVM.cs
+++ WPFWeatherApp/ViewModel/WeatherVM.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,10 +12,35 @@ namespace WPFWeatherApp.ViewModel
     {
         public AccuWeather Weather { get; set; }
 
+        private string query;
+
+        public string Query
+        {
+            get { return query; }
+            set
+            {
+                query = value;
+                GetCities();
+            }
+        }
+
+        public ObservableCollection<City> Cities { get; set; }
+
         public WeatherVM()
         {
             Weather = new AccuWeather();
+            Cities = new ObservableCollection<City>();
         }
 
+        private async void GetCities()
+        {
+            var cities = await WeatherAPI.GetAutoCompleteAsync(Query);
+
+            Cities.Clear();
+            foreach (var city in cities)
+            {
+                Cities.Add(city);
+            }
+        }
     }
 }
